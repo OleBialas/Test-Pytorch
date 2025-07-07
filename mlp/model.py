6ffb666841e20860d2da5c22cd5bf53dfcd93581
Ole Bialas <ole.bialas@posteo.de>
2025-07-07 13:55:14 +0200

    cleanup

--- mlp/model.py
+++ mlp/model.py
@@ -1,20 +1,7 @@
 # %%
-import os
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from load_data import test_dataloader, train_dataloader, labels_map, plot_random_images
-%load_ext autoreload
-%autoreload 2
 
-# Test importing worked
-#%%
-plot_random_images()
-
-#%%
-device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-print(f"Using {device} device")
 
 # %%
 class NeuralNetwork(nn.Module):
@@ -22,7 +9,7 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(28 * 28, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
@@ -33,77 +20,3 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-# %%
-model = NeuralNetwork()
-print(model)
-# %%
-X, _train_labels = next(iter(train_dataloader))
-X = X.to(device)
-logits = model(X)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
-# %%
-print(pred_probab)
-# %%
-loss = torch.nn.functional.binary_cross_entropy_with_logits(pred_probab.to('cpu'), _train_labels)
-loss
-
-#%%
-learning_rate = 1e-3
-batch_size = 64
-epochs = 5
-# %%
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-loss_fn = nn.CrossEntropyLoss()
-# %%
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        pred = pred.to('cpu')
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * batch_size + len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-def test_loop(dataloader, model, loss_fn):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    model.eval()
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
-    with torch.no_grad():
-        for X, y in dataloader:
-            print(X.shape)
-            pred = model(X)
-            print(pred)
-            pred = pred.to("cpu")
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-# %%
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, loss_fn)
-print("Done!")
-# %%
